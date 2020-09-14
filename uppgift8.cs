bd67673b8606dc4ae3cbf385c074e44abfcd12b5
bedudoorisr <71096981+bedudoorisr@users.noreply.github.com>
2020-09-14 23:04:19 +0200

    Add files via upload

---
+++ uppgift8.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Uppgift8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int siffra = 1;
+            for (int i = 1; i <= 16; i++)
+            {
+                siffra = i * i;
+                Console.WriteLine(siffra);
+            }
+
+        }
+    }
+}
