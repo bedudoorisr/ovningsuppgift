fa6db9f6dde15695d5a01e2dfd5900dfcf7d4d0f
bedudoorisr <71096981+bedudoorisr@users.noreply.github.com>
2020-09-14 23:07:06 +0200

    Add files via upload

---
+++ uppgifter10.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace uppgifter10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Mata in ett ord: ");
+            string inmatat = Console.ReadLine();
+
+            for (int i = 0; i < inmatat.Length; i++)
+            {
+                Console.WriteLine(inmatat[i]);
+            }
+        }
+    }
+}
+
