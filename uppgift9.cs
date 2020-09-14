630cde7849ab0ff4e5b995b04aa2006f028fe1a3
bedudoorisr <71096981+bedudoorisr@users.noreply.github.com>
2020-09-14 23:04:47 +0200

    Add files via upload

---
+++ uppgift9.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Uppgift9
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            Random random = new Random();
+            int randomNummer = random.Next(1, 100);
+
+
+            Console.WriteLine("Skriv in ett heltal");
+            int.TryParse(Console.ReadLine(), out int userInput);
+
+            while (userInput != randomNummer)
+            {
+
+                if (userInput < randomNummer)
+                {
+                    Console.WriteLine("du får gissa högre");
+
+                    int.TryParse(Console.ReadLine(), out userInput);
+                }
+
+                if (userInput > randomNummer)
+                {
+
+                    Console.WriteLine("du får gissa lägre");
+                    int.TryParse(Console.ReadLine(), out userInput);
+                }
+            }
+            Console.WriteLine("Wow du vann");
+        }
+    }
+}
