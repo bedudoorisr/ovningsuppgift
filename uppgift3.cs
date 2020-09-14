7b03bfacb2061a364a69271c62d8033cd3ef2de8
bedudoorisr <71096981+bedudoorisr@users.noreply.github.com>
2020-09-14 23:02:49 +0200

    Add files via upload

---
+++ uppgift3.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace uppgift3c
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Skriv in ett tal: ");
+            int inmatatTal = int.Parse(Console.ReadLine());
+
+            if (inmatatTal < 100)
+            {
+                Console.WriteLine("Talet du skrev in är mindre än 100");
+            }
+            else if (inmatatTal == 100)
+            {
+                Console.WriteLine("talet du skrev in är lika med 100");
+            }
+            else
+            {
+                Console.WriteLine("talet du skrev in är större än 100");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
