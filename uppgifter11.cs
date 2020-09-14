7ae351afc6d202b2bc128e9ed0743a80a812214a
bedudoorisr <71096981+bedudoorisr@users.noreply.github.com>
2020-09-14 23:08:01 +0200

    Add files via upload

---
+++ uppgifter11.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace uppgifter11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] array = new string[10] { "noll", "ett", "två", "tre", "fyra", "fem", "sex", "sju", "åtta", "nio" };
+
+            Console.Write("Ange en siffra mellan 0 till 9: ");
+            int inmatat = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(array[inmatat]);
+
+        }
+    }
+}
