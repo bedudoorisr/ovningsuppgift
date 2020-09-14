d0d33f4416b12eaae475b8e33e71e4f234edcf8c
bedudoorisr <71096981+bedudoorisr@users.noreply.github.com>
2020-09-14 23:09:05 +0200

    Add files via upload

---
+++ uppgift12.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+
+namespace Uppgift12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            int antalTal;
+            int[] a = new int[100];
+
+            Console.Write("Hur många tal vill du mata in? :");
+            antalTal = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+
+            for (i = 1; i < antalTal + 1; i++)
+            {
+                Console.Write("Ange tal {0}: ", i);
+                a[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.Write("\nDu matade in: \n");
+            for (i = 1; i < antalTal + 1; i++)
+            {
+                Console.Write("{0}  ", a[i]);
+            }
+
+            Console.Write("\n\nTalen i omvänd ordning:\n");
+            for (i = antalTal; i >= 1; i--)
+            {
+                Console.Write("{0} ", a[i]);
+            }
+            Console.Write("\n\n");
+        }
+    }
+}
