a5ce697e58f3628b7a7b68ffb91c7ce36c8197e6
bedudoorisr <71096981+bedudoorisr@users.noreply.github.com>
2020-09-14 23:10:18 +0200

    Add files via upload

---
+++ uppgift13.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace Uppgift13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Mata in din text: ");
+            string userInput = Console.ReadLine();
+
+            string resultString = string.Join(" ", userInput.Split(' ').Select(x => new string(x.Reverse().ToArray())));
+
+            Console.WriteLine("din text i omvänd ordning :" + resultString);
+
+        }
+    }
+}
