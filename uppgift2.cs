11aedf029a4714285f5223f3ce633f2491d8cf58
bedudoorisr <71096981+bedudoorisr@users.noreply.github.com>
2020-09-14 23:02:22 +0200

    Add files via upload

---
+++ uppgift2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Uppgift2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                string hemligLösenord = "zaq12wsx";
+
+                Console.Write("Skriv in ett lösenord: ");
+                string userLösenord = Console.ReadLine();
+
+                if (userLösenord == hemligLösenord)
+                {
+                    Console.WriteLine("Du skrev in rätt lösenord!");
+                }
+                else
+                {
+                    Console.WriteLine("Du skrev in fel lösenord.");
+                }
+
+                Console.ReadKey();
+            }
+        }
+    }
+}m
