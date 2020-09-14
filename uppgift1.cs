7202e63a928fed3fc4b7e23394a1277bdb73c9f0
bedudoorisr <71096981+bedudoorisr@users.noreply.github.com>
2020-09-14 23:01:38 +0200

    Add files via upload

---
+++ uppgift1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Uppgift1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Mata in ditt namn: ");
+            string UserName = Console.ReadLine();
+            Console.WriteLine("Hej " + UserName + "!");
+            Console.ReadKey();
+        }
+    }
+}
